9f08ea2a11e2095a628a9a4b5202b07c8cd629d3
Deiby Montoya <deiby.sk@hotmail.com>
2022-12-03 16:55:14 -0500

    Fix reaload pages. Add FontAwesome.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,33 +2,14 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 
 // Pages
-import { Clients, HomePage, NotFound, Orders, Products } from './pages/Index';
+import { NavBar } from './components/nav-bar';
 
 export const App = () =>
   <div className='container'>
-    <Nav className="justify-content-center" activeKey="/">
-        <Nav.Item>
-          <Nav.Link href="/">Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/products">Products</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/customers">Customers</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/orders">Orders</Nav.Link>
-        </Nav.Item>
-      </Nav>
-      <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/customers" element={<Clients />} />
-      <Route path="/orders" element={<Orders />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <NavBar />
   </div>
 
 export default App;
---
+++ client/src/components/nav-bar.tsx
@@ -0,0 +1,42 @@
+import { Route, Routes, Link } from 'react-router-dom';
+import './nav-bar.css'
+import Nav from 'react-bootstrap/Nav';
+import { Clients, HomePage, NotFound, Orders, Products } from '../pages/Index';
+import { Navbar } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouseLaptop } from "@fortawesome/free-solid-svg-icons";
+
+export const NavBar = () => {
+  return (
+    <>
+      <Nav className="justify-content-center mt-4" activeKey="/">
+        <Navbar.Brand>
+          <Link to="/" className="cursor-pointer mr-4">
+            <FontAwesomeIcon icon={faHouseLaptop} />
+          </Link>
+        </Navbar.Brand>
+        <Nav.Item className='mr-2'>
+          <Link to="/">Home</Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to="/products">Products</Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to="/customers">Customers</Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to="/orders">Orders</Link>
+        </Nav.Item>
+      </Nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/customers" element={<Clients />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  )
+}
+
+
