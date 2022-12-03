7b77f71f56e213fa16d3fa885179609c36250dda
pipes31 <felipe961031@gmail.com>
2022-12-03 17:30:08 -0500

    clients v2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 // Pages
-import { Clients, HomePage, NotFound, Orders, Products } from './pages/Index';
+import { Clients, HomePage, NotFound, Orders, Products } from "./pages/Index";
 
-export const App = () =>
-  <div className='container'>
-    <Nav className="justify-content-center" activeKey="/">
+export const App = () => (
+  <div className="container">
+    <Navbar bg="dark" variant="dark">
+      <Nav className="justify-content-center" activeKey="/">
         <Nav.Item>
           <Nav.Link href="/">Home</Nav.Link>
         </Nav.Item>
@@ -22,7 +24,8 @@ export const App = () =>
           <Nav.Link href="/orders">Orders</Nav.Link>
         </Nav.Item>
       </Nav>
-      <Routes>
+    </Navbar>
+    <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/products" element={<Products />} />
       <Route path="/customers" element={<Clients />} />
@@ -30,5 +33,6 @@ export const App = () =>
       <Route path="*" element={<NotFound />} />
     </Routes>
   </div>
+);
 
 export default App;
