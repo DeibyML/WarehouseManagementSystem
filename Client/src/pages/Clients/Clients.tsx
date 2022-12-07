ba7fa3755a417cb276a720967625734d0dc97abd
pipes31 <felipe961031@gmail.com>
2022-12-07 13:19:00 -0500

    final changes

--- Client/src/pages/Clients/Clients.tsx
+++ Client/src/pages/Clients/Clients.tsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { ClientTable } from "../../components/ClientsComponents/clientTable";
-//import { CustomerTable } from "../components/customerTable";
 
 export const Clients = () => {
   return <ClientTable />;
