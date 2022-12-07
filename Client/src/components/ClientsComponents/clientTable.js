a72eb5b73dd2ce83ff44d40c84bdc2fcee79f3ae
pipes31 <felipe961031@gmail.com>
2022-12-07 13:14:05 -0500

    Create and Update ClientTable

--- Client/src/components/ClientsComponents/clientTable.js
+++ Client/src/components/ClientsComponents/clientTable.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Constants } from "../../constants";
 import axios from "axios";
-//import { Client } from "../models/client";
+
 import CRUDTable, {
   Fields,
   Field,
@@ -12,24 +12,36 @@ import CRUDTable, {
 import "../table.css";
 
 export const ClientTable = () => {
-  const [client, setClients] = useState([]);
+  const servidor = Constants.SERVER_URL + Constants.CONTROLLER_CLIENT;
+  let [clients, setClients] = useState([]);
+  const [status, setStatus] = useState();
+  const [name, setName] = useState("");
+  const [telephone, setTelephone] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [city, setCity] = useState("");
+  const [province, setProvince] = useState("");
 
+  //Get Customers from Server
   useEffect(() => {
-    axios
-      .get(Constants.SERVER_URL + Constants.CONTROLLER_CLIENT)
-      .then((res) => {
-        console.log("Data cargada", res.data);
-        setClients(res.data);
-      });
+    const resp = async () => await axios.get(servidor);
+    resp().then((res) => setClients(res.data));
   }, []);
-  let clientes = client.map((data, index) => {
-    return (
-      <>
-        {data.id},{data.name}
-      </>
-    );
-  });
-  let clients = { clientes };
+
+  const header = {
+    id: 3,
+    name: "Gato",
+    telephone: "123456",
+    email: "deiby@gmail.com",
+    address: "1484 valuevillage",
+    postalCode: "L5S",
+    city: "Mississsauga",
+    province: "ON",
+  };
+  function Enviar() {
+    alert("Hello!");
+  }
 
   const SORTERS = {
     NUMBER_ASCENDING: (mapper) => (a, b) => mapper(a) - mapper(b),
@@ -64,28 +76,48 @@ export const ClientTable = () => {
       result = result.sort(getSorter(payload.sort));
       return Promise.resolve(result);
     },
-    create: (client) => {
-      count += 1;
-      clients.push({
-        ...client,
-        id: count,
+    create: async (client) => {
+      try {
+        // Assing new Id random
+        let newId = Math.floor(Math.random() * 1000);
+
+        // In case to be able to calculate next id, it adds +1 from the last item.
+        if (!!Number(clients[clients.length - 1]?.id)) {
+          newId = Number(clients[clients.length - 1]?.id) + 1;
+        }
+        client.id = newId;
+        await axios.post(servidor, client).then(() => {
+          clients.push({
+            ...client,
+          });
+        });
+      } catch (error) {
+        console.error(error.response.data);
+      }
+
+      return Promise.resolve(client);
+    },
+    update: async (data) => {
+      const client = clients.find((t) => t.id === data.id);
+      await axios.put(servidor, data).then(() => {
+        client.name = data.name;
+        client.telephone = data.telephone;
+        client.email = data.email;
+        client.address = data.address;
+        client.postalCode = data.postalCode;
+        client.city = data.city;
+        client.province = data.province;
       });
+
       return Promise.resolve(client);
     },
-    update: (data) => {
+    delete: async (data) => {
       const client = clients.find((t) => t.id === data.id);
-      client.name = data.name;
-      client.telephone = data.telephone;
-      client.email = data.email;
-      client.address = data.address;
-      client.postalCode = data.postalCode;
-      client.city = data.city;
-      client.province = data.province;
-      return Promise.resolve(client);
-    },
-    delete: (data) => {
-      const client = clients.find((t) => t.id === data.id);
-      clients = clients.filter((t) => t.id !== client.id);
+      clients = clients.filter((t) => t._id !== client._id);
+      await axios.delete(servidor, { data: { _id: data._id } }).then((resp) => {
+        console.log("Server response: " + resp.message);
+      });
+
       return Promise.resolve(client);
     },
   };
@@ -126,125 +158,68 @@ export const ClientTable = () => {
 
   return (
     <div style={styles.container}>
-      <CRUDTable
-        caption="Clients"
-        fetchItems={(payload) => service.fetchItems(payload)}
-      >
-        <Fields>
-          <Field name="id" label="Id" hideInCreateForm hideInUpdateForm />
-          <Field name="name" label="Name" placeholder="Name" />
-          <Field name="telephone" label="Telephone" placeholder="123456789" />
-          <Field name="email" label="Email" placeholder="mail@mail.com" />
-          <Field name="address" label="Address" placeholder="address" />
-          <Field name="postalCode" label="Postal Codel" placeholder="XXX XXX" />
-          <Field name="city" label="City" placeholder="City" />
-          <Field name="province" label="Province" placeholder="Province" />
-        </Fields>
-        <CreateForm
-          title="New Client"
-          message="Add a new Client"
-          trigger="Add Client"
-          onSubmit={(client) => service.create(client)}
-          submitText="ADD"
-          validate={validation}
-        />
+      {clients?.length > 0 && (
+        <CRUDTable
+          caption="Clients"
+          fetchItems={(payload) => service.fetchItems(payload)}
+        >
+          <Fields>
+            <Field name="id" label="Id" hideInUpdateForm hideInCreateForm />
+            <Field name="name" label="Name" placeholder="Name" />
+            <Field name="telephone" label="Telephone" placeholder="123456789" />
+            <Field name="email" label="Email" placeholder="mail@mail.com" />
+            <Field name="address" label="Address" placeholder="address" />
+            <Field
+              name="postalCode"
+              label="Postal Codel"
+              placeholder="XXX XXX"
+            />
+            <Field name="city" label="City" placeholder="City" />
+            <Field name="province" label="Province" placeholder="Province" />
+          </Fields>
 
-        <UpdateForm
-          title="Client Update"
-          message="Update Client"
-          trigger="Update"
-          onSubmit={(client) => service.update(client)}
-          submitText="Update"
-          validate={validation}
-        />
+          <CreateForm
+            title="New Product"
+            message="Add a new Product"
+            trigger="Add Product"
+            onSubmit={(product) => {
+              service.create(product);
+              window.location.reload();
+            }}
+            submitText="ADD"
+            validate={validation}
+          />
+          <UpdateForm
+            title="Client Update"
+            message="Update Client"
+            trigger="Update"
+            onSubmit={(client) => {
+              service.update(client);
+              window.location.reload();
+            }}
+            submitText="Update"
+            validate={validation}
+          />
 
-        <DeleteForm
-          title="Client Delete"
-          message="Are you sure you want to delete this Client"
-          trigger="Delete"
-          onSubmit={(client) => service.delete(client)}
-          submitText="Delete"
-          validate={(values) => {
-            const errors = {};
-            if (!values.id) {
-              errors.id = "Please, provide id";
-            }
-            return errors;
-          }}
-        />
-      </CRUDTable>
+          <DeleteForm
+            title="Client Delete"
+            message="Are you sure you want to delete this Client"
+            trigger="Delete"
+            onSubmit={(client) => {
+              service.delete(client);
+              window.location.reload();
+            }}
+            submitText="Delete"
+            validate={(values) => {
+              const errors = {};
+              if (!values.id) {
+                errors.id = "Please, provide id";
+              }
+              return errors;
+            }}
+          />
+        </CRUDTable>
+      )}
     </div>
   );
 };
-
-/*import { useTable, useSortBy, usePagination } from "react-table";
-import useRows from "./rowsClients";
-import useColumns from "./columsClient";
-
-
-export const ClientTable = () => {
-  const columns = useColumns();
-  const data = useRows();
-  const table = useTable({ columns, data }, useSortBy, usePagination);
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    page,
-    nextPage,
-    previousPage,
-    canNextPage,
-    canPreviousPage,
-    prepareRow,
-  } = table;
-  return (
-    <>
-      <table {...getTableProps}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
-                  className={
-                    column.isSorted
-                      ? column.isSortedDesc
-                        ? "desc"
-                        : "asc"
-                      : ""
-                  }
-                >
-                  {column.render("Header")}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-
-        <tbody {...getTableBodyProps()}>
-          {page.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <div>
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-          Previous
-        </button>
-        <button onClick={() => nextPage()} disabled={!canNextPage}>
-          Next
-        </button>
-      </div>
-    </>
-  );
-}; */
